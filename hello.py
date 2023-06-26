f1a034d67e5dab4e92cad62c43947240540aa2f7
sergiozap <sergiozap1@gmail.com>
2023-06-26 21:07:10 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+# This is the first comment
+
+# Hola Mundo: 
+
+"""
+Esto es un comentario 
+en varias lineas de código 
+"""
+print("Hello World!")
+
+# Day 1 of the 30DaysOfPython Challenge
+print(2+2) #addition
+print(3-1) # substraction
+print(3*2) # multiplication
+print(3/2) # division
+print(3**2) #exponential
+print(3%2) # modulus 
+print(3//2) # floor division operator. it gets the integer division
+
+print(type(10))
+print(type(3.14))
+print(type('Sergio'))
+
