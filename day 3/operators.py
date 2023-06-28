b8c02fdeb3d76a980c31e6209a28a7776ba64f23
sergiozap <sergiozap1@gmail.com>
2023-06-28 20:03:31 +0200

    day 3

---
+++ day 3/operators.py
@@ -0,0 +1,64 @@
+import math
+age, height, complex_number = 21, 175, 1j
+
+# base = input('Enter the base of the triangle: ')
+# height_user = input('Height of the triangle: ')
+# triangle_area = 0.5 * float(base) * float(height_user)
+# print('The area of the triangle is: ', triangle_area)
+
+# side_a = input('Side a of the triangle')
+# side_b = input('Side b of the triangle')
+# side_c = input('Side c of the triangle')
+# perimeter = side_a + side_b + side_c
+# print('Perimeter of the triangle is: ', perimeter)
+
+# length = input('Length of the rectangle: ')
+# width = input('Width of the rectangle: ')
+# area = float(length) * float(width)
+# rectangle_perimeter = 2 * (length + width)
+# print('Perimeter of the rectangle is: ', rectangle_perimeter)
+
+# radius = input('Radius of the circle: ')
+# circle_area = math.pi * pow(float(radius), 2)
+# circle_circumference = 2 * math.pi * float(radius)
+# print('Area of the circle is: ', round(circle_area, 2))
+
+python_length = len('python')
+dragon_length = len('dragon')
+print(dragon_length < python_length)
+
+print('on' in 'dragon' and 'on' in 'python')
+
+print('jargon' in 'I hope this course is not full of jargon.' )
+
+print('on' not in 'dragon' and 'on' not in 'python')
+python_length_float = float(python_length)
+python_length_string = str(python_length_float)
+print(python_length_string)
+
+if(4%2 == 0):
+    print('the number is even')
+else:   print('the number is odd')
+
+print(7//3 == int(2.7))
+
+print(type('10') == type(10))
+
+print(int(9.8) == 10) # false 9 != 10
+
+# script for weekly earnings
+# hours = input('Enter hours: ')
+# rate_per_hour = input('Enter rate per hour: ')
+# print('Your weekly earning is ', int(hours) * int(rate_per_hour))
+
+# script to calculate the numbers of seconds a person can live
+# years_lived = min(int(input('Enter number of years you have lived: ')),100)
+# print('You have lived for', int(years_lived)*365*24*3600, 'seconds')
+
+#script to display a table
+print("1 1 1 1 1")
+print("2 1 2 4 8")
+print("3 1 3 9 27")
+print("4 1 4 16 64")
+print("5 1 5 25 125")
+
